431e4fdf8684c55d48a7c591568934b818cafe45
Vishal <vishalvk38879@gmail.com>
2022-07-21 00:06:31 +0530

    Creating and deleting posts

--- website/models.py
+++ website/models.py
@@ -1,5 +1,3 @@
-import email
-from enum import unique
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
@@ -10,4 +8,12 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    
+    posts = db.relationship('Post', backref='user', passive_deletes=True)
+
+class Post(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    text= db.Column(db.Text, nullable= False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+
+
