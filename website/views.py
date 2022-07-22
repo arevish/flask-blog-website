a5d810bae3eb7204b48959bb23730071c20ab15e
Vishal <vishalvk38879@gmail.com>
2022-07-22 18:09:06 +0530

    Adding and Deleting Comments

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, flash, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post, User 
+from .models import Post, User , Comment
 from. import db
 
 views = Blueprint("views", __name__)
@@ -53,3 +53,34 @@ def posts(username):
 
     posts = user.posts
     return render_template("posts.html", user=current_user, posts=posts, username=username)
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    text=request.form.get('text')
+
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment= Comment(text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exits.', category='error')
+    return redirect(url_for('views.home'))
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+    if not comment:
+        flash('Comment does not exists.', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permision to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('views.home'))
