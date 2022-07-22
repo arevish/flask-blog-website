bc2c33475bfb5f2120ccadc5fc4a8a5522252232
Vishal <vishalvk38879@gmail.com>
2022-07-22 22:32:04 +0530

    Like post functionality

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, flash, render_template, request, redirect, url_for
+from flask import Blueprint, flash, render_template, request, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-from .models import Post, User , Comment
+from .models import Post, User , Comment, Like
 from. import db
 
 views = Blueprint("views", __name__)
@@ -84,3 +84,20 @@ def delete_comment(comment_id):
         db.session.commit()
 
     return redirect(url_for('views.home'))
+
+@views.route("/like-post/<post_id>", methods=['POST'])
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    like= Like.query.filter_by(author=current_user.id, post_id=post_id).first()
+    if not post:
+        return jsonify({'error': 'Post does not exists.'},400)
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like= Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+    
+    return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author, post.likes)})
